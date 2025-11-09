4df2a3c5a6cdb82889b1d8196e1bba4ae6133fa7
SAWARATSUKI <nametama@outlook.jp>
2025-11-09 09:06:11 +0900

    fix: page.tsxの不要なコンポーネントの削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 // app/page.tsx
-import { main } from "framer-motion/client";
-import News4003 from "./components/News/News4003";
 import Price4003 from "./components/Price/Price4003";
 
 export default function Page() {
