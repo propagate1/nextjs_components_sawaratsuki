6ba41e0e4cff51b3498ccd8f5f28dc18f0736dba
SAWARATSUKI <nametama@outlook.jp>
2025-11-19 15:00:37 +0900

    add: Voiceを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 // app/page.tsx
 import Service4010 from "./components/Service/Service4010";
+import Voice4011 from "./components/Voice/Voice4011";
 export default function Page() {
   return (
     <main className="min-h-screen">
-      <Service4010 />
+      <Voice4011 />
     </main>
   );
 }
