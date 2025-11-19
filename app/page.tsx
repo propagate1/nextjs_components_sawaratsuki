b40c7103df3686c89ecc9f0217f5e40e854dbba8
SAWARATSUKI <nametama@outlook.jp>
2025-11-19 15:50:22 +0900

    Add: 実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import MiddleCTA4012 from "./components/CTA/MiddleCTA4012";
+
 // app/page.tsx
-import Service4010 from "./components/Service/Service4010";
-import Voice4011 from "./components/Voice/Voice4011";
 export default function Page() {
   return (
     <main className="min-h-screen">
-      <Voice4011 />
+      <MiddleCTA4012 />
     </main>
   );
 }
